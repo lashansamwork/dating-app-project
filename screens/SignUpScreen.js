e0a8573c96b562b26eca25b26cef624a3fa0708b
daminduLiyanage <damindu7@gmail.com>
2021-01-14 18:31:54 +0530

    content added without styles

--- screens/SignUpScreen.js
+++ screens/SignUpScreen.js
@@ -1,9 +1,28 @@
-import React from 'react';
-import { Text } from 'react-native';
+import React, { useState } from 'react';
+import { View } from 'react-native';
+import { Text, TextInput, Button } from 'react-native-paper';
 
 const SignUpScreen = () => {
+
+    const [name, setName] = useState('');
     return (
-        <Text>SignUpScreen</Text>
+        <View>
+            <Text>Dating Lanka</Text>
+            <TextInput
+                label="Your name for others to see"
+                onChange={text => setName(text)}
+                mode="outlined"
+            />
+            <Text> Upload your picture</Text>
+            <Text>Drag and Drop Picture</Text>
+            <Text>We will use the image you provided for verification purposes</Text>
+            <Text>I am a </Text>
+            <Text>Guy</Text>
+            <Text>|</Text>
+            <Text>Girl</Text>
+            <Text>By clicking on continue you will agree to our terms and conditions</Text>
+            <Button>Continue</Button>
+        </View>
     );
 }
 
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,13 +1,28 @@
-import React from 'react';
+import React,  { useState } from 'react';
 import {View} from 'react-native';
-import {Button} from 'react-native-paper';
+import {Button, Text, TextInput} from 'react-native-paper';
 
 const WelcomeScreen = ({navigation}) => {
+
+    const [phoneNumber, setPhoneNumber] = useState('+94');
+
     return(
         <View>
+            <Text>Dating Lanka</Text>
+            <Text>Welcome to Dating Lanka</Text>
+            <Text>Please verify your phone number</Text>
+            <TextInput
+                label="phone number"
+               // value="+94"
+                onChangeText={text => setPhoneNumber(text)}
+            />
             <Button onPress={() => navigation.navigate("SignUpScreen")} >
-                Go to Sign Up
+                [Temp] Go to Sign Up
             </Button>
+            
+            <Button icon="google" />
+            <Button icon="facebook" />
+
         </View>
     );
 }
